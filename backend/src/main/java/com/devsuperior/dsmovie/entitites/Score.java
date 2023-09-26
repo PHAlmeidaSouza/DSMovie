bb908637a2713a93bd600a9c1847d51ebbb4e5e2
Pedro Henrique <ph8584774@gmail.com>
2023-09-26 20:57:27 -0300

    Domain model, database seed

---
+++ backend/src/main/java/com/devsuperior/dsmovie/entitites/Score.java
@@ -0,0 +1,42 @@
+package com.devsuperior.dsmovie.entitites;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_score")
+public class Score {
+	
+	@EmbeddedId
+	private ScorePK id = new ScorePK();
+	private Double Value;
+
+	public void setMovie(Movie movie) {
+		id.setMovie(movie);
+	}
+	
+	public void setUser(User user) {
+		id.setUser(user);
+	}
+
+	public Score() {
+	}
+
+	public ScorePK getId() {
+		return id;
+	}
+
+	public void setId(ScorePK id) {
+		this.id = id;
+	}
+
+	public Double getValue() {
+		return Value;
+	}
+
+	public void setValue(Double value) {
+		Value = value;
+	}
+
+}
