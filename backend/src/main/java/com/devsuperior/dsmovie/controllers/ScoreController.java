e50a04c95f4bc76caf947bb9d5f6a76ba6311d6a
Pedro Henrique <ph8584774@gmail.com>
2023-09-27 13:17:15 -0300

    Save Score

--- backend/src/main/java/com/devsuperior/dsmovie/controllers/ScoreController.java
+++ backend/src/main/java/com/devsuperior/dsmovie/controllers/ScoreController.java
@@ -1,5 +1,6 @@
 package com.devsuperior.dsmovie.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,13 +8,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dsmovie.dto.MovieDTO;
 import com.devsuperior.dsmovie.dto.ScoreDTO;
+import com.devsuperior.dsmovie.services.ScoreService;
 
 @RestController
 @RequestMapping(value = "/scores")
 public class ScoreController {
-
+	
+	@Autowired
+	private ScoreService service;	
+	
 	@PutMapping
 	public MovieDTO saveScore(@RequestBody ScoreDTO dto) {
-		return null;
+		MovieDTO movieDTO = service.saveScore(dto);
+		return movieDTO;
 	}
 }
--- backend/src/main/java/com/devsuperior/dsmovie/services/ScoreService.java
+++ backend/src/main/java/com/devsuperior/dsmovie/services/ScoreService.java
@@ -1,9 +1,10 @@
 package com.devsuperior.dsmovie.services;
 
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dsmovie.dto.MovieDTO;
 import com.devsuperior.dsmovie.dto.ScoreDTO;
 import com.devsuperior.dsmovie.entities.Movie;
 import com.devsuperior.dsmovie.entities.Score;
@@ -25,7 +26,7 @@ public class ScoreService {
 	private ScoreRepository scoreRepository;
 
 	@Transactional
-	public void saveScore(ScoreDTO dto) {
+	public MovieDTO saveScore(ScoreDTO dto) {
 		User user = userRepository.findByEmail(dto.getEmail());
 		if (user == null) {
 			user = new User();
@@ -34,37 +35,26 @@ public class ScoreService {
 		}
 
 		Movie movie = movieRepository.findById(dto.getMovieId()).get();
-		
+
 		Score score = new Score();
 		score.setMovie(movie);
 		score.setUser(user);
 		score.setValue(dto.getScore());
-		
+
 		score = scoreRepository.saveAndFlush(score);
-		
+
 		double sum = 0.0;
 		for (Score s : movie.getScores()) {
 			sum += s.getValue();
 		}
-		
+
 		double avg = sum / movie.getScores().size();
-		
+
 		movie.setScore(avg);
 		movie.setCount(movie.getScores().size());
-		
+
 		movie = movieRepository.save(movie);
+
+		return new MovieDTO(movie);
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
